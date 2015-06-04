8a4339be9b288ffc7661120a850ecd74df9c01d4
Gandiko <ngander@me.com>
2015-06-04 18:38:06 +0200

    Weiter getestet
    Problem: Die Funktionen post funktionieren nicht richtig - Es wird nur
    die ID initialisiert

--- test-test/app.js
+++ test-test/app.js
@@ -33,7 +33,7 @@ app.get('/users/:id', function(req, res){
         });
     });
 
-app.put('/user/:id', function(req, res){
+app.put('/users/:id', function(req, res){
     db.exists('user:'+req.params.id, function(err, rep){
         if (rep == 1){
             var updatedUser = req.body;
@@ -43,9 +43,55 @@ app.put('/user/:id', function(req, res){
             });
         }
         else {
-            res.status(404).type('text').send('Der User mit der ID' + req.params.id + 'wurde nicht gefunden');
+            res.status(404).type('text').send('Der User mit der ID ' + req.params.id + ' wurde nicht gefunden');
         }
     });  
 });
+
+app.delete('/users/:id', function(req, res){
+    db.del('user:'+req.params.id, function(err, rep){
+        if(rep == 1){
+            res.status(200).type('text').send('OK');
+        }
+        else{
+            res.status(404).type('text').send('Der User mit der ID ' + req.params.id + ' wurde nicht gefunden');
+        }
+    }); 
+});
+
+app.get("/users", function(req, res){
+    db.keys('user:*', function(err, rep){
+        var users = []; 
+        
+        if(rep.length == 0) {
+            res.json(users);
+            return;
+        }
+        
+        db.mget(rep, function(err, rep){
+           rep.forEach(function(val){
+               users.push(JSON.parse(val));
+           });
+        
+            users = users.map(function(user){
+                return {id: user.id, name: user.name};
+            });
+            res.json(users);
+        });
+    }); 
+});
+
+
+app.post('/equipment/kamera', function(req, res){
+        var newCam = req.body;
+    
+        db.incr('id:kamera', function(err, rep){
+            newCam.id = rep;
+            db.set('kamera:' + newCam.id, JSON.stringify(newCam), function(err, rep){
+                res.json(newCam); 
+            });
+        });
+    });
+
 
 app.listen(3000);
